174c7a06ec9c3af8498826fe13a8ecf960dff07d
TopGun1405 <fstopgun0317@gmail.com>
2022-06-13 22:36:50 +0900

    BaekJoonOJ_N-2744

---
+++ BOJ_N-2744.py
@@ -0,0 +1,12 @@
+def main():
+    c = input()
+    print(c.swapcase())
+    # for w in c:
+    #     if w.isupper():
+    #         print(w.lower(), end='')
+    #     elif w.islower():
+    #         print(w.upper(), end='')
+
+
+if __name__ == "__main__":
+    main()
