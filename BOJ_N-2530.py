aaa13d17bf212c68efed77216a4b85ce9da56164
TopGun1405 <fstopgun0317@gmail.com>
2022-06-24 22:16:51 +0900

    BaekJoonOJ_N-2530

---
+++ BOJ_N-2530.py
@@ -0,0 +1,18 @@
+def main():
+    A, B, C = map(int, input().split())
+    D = int(input())
+
+    C += D
+    if C > 59:
+        B += C // 60
+        C = C % 60
+    if B > 59:
+        A += B // 60
+        B = B % 60
+    if A > 23:
+        A = A % 24
+    print(A, B, C)
+
+
+if __name__ == "__main__":
+    main()
