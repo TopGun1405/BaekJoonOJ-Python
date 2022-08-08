0ecf2d68a2e55045495e1aab99fb49de694d7c7c
TopGun1405 <fstopgun0317@gmail.com>
2022-08-08 23:03:21 +0900

    BaekJoonOJ_N-15650

---
+++ BOJ_N-15650.py
@@ -0,0 +1,23 @@
+def main():
+    N, M = map(int, input().split())
+    t = []
+    dfs(t, N, M)
+
+
+def dfs(s, n, m):
+    # #########
+    if len(s) == m:
+        print(' '.join(map(str, s)))
+        return
+    for i in range(1, n + 1):
+        if i in s:
+            continue
+        if len(s) > 0 and i < s[-1]:
+            continue
+        s.append(i)
+        dfs(s, n, m)
+        s.pop()
+
+
+if __name__ == "__main__":
+    main()
