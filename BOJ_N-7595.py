4a76b231cfa92aa5e0a2f880fb55b861baca4c0f
TopGun1405 <fstopgun0317@gmail.com>
2022-10-09 20:55:48 +0900

    BaekJoonOJ_N-7595

---
+++ BOJ_N-7595.py
@@ -0,0 +1,11 @@
+def main():
+    while True:
+        n = int(input())
+        if n == 0:
+            break
+        for i in range(n):
+            print('*' * (i + 1))
+
+
+if __name__ == "__main__":
+    main()
