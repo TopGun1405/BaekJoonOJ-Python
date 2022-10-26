14c777a23884d318911ed22e07199b398f3fd2d2
TopGun1405 <fstopgun0317@gmail.com>
2022-10-26 23:28:02 +0900

    BaekJoonOJ_N-2563

---
+++ BOJ_N-2563.py
@@ -0,0 +1,16 @@
+def main():
+    P = int(input())
+    paper = [[0 for _ in range(101)] for _ in range(101)]
+    for _ in range(P):
+        x, y = map(int, input().split())
+        for i in range(x, x + 10):
+            for j in range(y, y + 10):
+                paper[i][j] = 1
+    ans = 0
+    for r in paper:
+        ans += r.count(1)
+    print(ans)
+
+
+if __name__ == "__main__":
+    main()
