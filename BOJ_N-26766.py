5fe3cb0e54a1fe4697048cdf45a62c872dc584f4
TopGun1405 <fstopgun0317@gmail.com>
2022-12-25 22:35:21 +0900

    BaekJoonOJ_N-26766

---
+++ BOJ_N-26766.py
@@ -0,0 +1,16 @@
+def main():
+    N = int(input())
+    for _ in range(N):
+        print(" @@@   @@@ ")
+        print("@   @ @   @")
+        print("@    @    @")
+        print("@         @")
+        print(" @       @ ")
+        print("  @     @  ")
+        print("   @   @   ")
+        print("    @ @    ")
+        print("     @     ")
+
+
+if __name__ == "__main__":
+    main()
