8b20871d72f1c968915f4df9786f6436de886efe
TopGun1405 <fstopgun0317@gmail.com>
2022-12-31 23:38:51 +0900

    BaekJoonOJ_N-26742

---
+++ BOJ_N-26742.py
@@ -0,0 +1,12 @@
+def main():
+    # calc = {'B': 0, 'C': 0}
+    # sks = input()
+    # for s in sks:
+    #     calc[s] += 1
+    # print(calc['B'] // 2 + calc['C'] // 2)
+    sks = input()
+    print(sks.count('B') // 2 + sks.count('C') // 2)
+
+
+if __name__ == "__main__":
+    main()
