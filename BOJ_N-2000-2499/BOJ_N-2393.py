722eb3c7e4521f4d4a174a553a0b7172afabb7bc
TopGun1405 <fstopgun0317@gmail.com>
2023-01-27 23:16:00 +0900

    BaekJoonOJ_N-2393

---
+++ BOJ_N-2000-2499/BOJ_N-2393.py
@@ -0,0 +1,37 @@
+#        IDENTIFICATION DIVISION.
+#        PROGRAM-ID. IDSAMPLE.
+#        ENVIRONMENT DIVISION.
+#        PROCEDURE DIVISION.
+#        DISPLAY '  ___  ___  ___'
+#        DISPLAY '  | |__| |__| |'
+#        DISPLAY '  |           |'
+#        DISPLAY '   \_________/'
+#        DISPLAY '    \_______/'
+#        DISPLAY '     |     |'
+#        DISPLAY '     |     |'
+#        DISPLAY '     |     |'
+#        DISPLAY '     |     |'
+#        DISPLAY '     |_____|'
+#        DISPLAY '  __/       \__'
+#        DISPLAY ' /             \'
+#        DISPLAY '/_______________\'
+#        STOP RUN.
+
+def main():
+    print('  ___  ___  ___')
+    print('  | |__| |__| |')
+    print('  |           |')
+    print('   \\_________/')
+    print('    \\_______/')
+    print('     |     |')
+    print('     |     |')
+    print('     |     |')
+    print('     |     |')
+    print('     |_____|')
+    print('  __/       \\__')
+    print(' /             \\')
+    print('/_______________\\')
+
+
+if __name__ == "__main__":
+    main()
