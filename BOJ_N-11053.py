3ddedafa52aa643c7e6e1bccf6ac2b7ab268f969
TopGun1405 <fstopgun0317@gmail.com>
2023-04-14 23:28:25 +0900

    BaekJoonOJ_N-11053

---
+++ BOJ_N-11053.py
@@ -0,0 +1,14 @@
+def main():
+    N = int(input())
+    A = list(map(int, input().split()))
+    length, maxLen = [1] * N, 1
+    for i in range(1, N):
+        for j in range(i):
+            if A[i] > A[j]:
+                length[i] = max(length[i], length[j] + 1)
+                maxLen = length[i] if length[i] > maxLen else maxLen
+    print(maxLen)
+
+
+if __name__ == "__main__":
+    main()
