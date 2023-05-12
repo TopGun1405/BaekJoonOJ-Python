7a1b58b91e6a9e6cc5168bb01ffaa3ca8e1c56af
TopGun1405 <fstopgun0317@gmail.com>
2023-05-12 20:49:38 +0900

    BaekJoonOJ_N-18247

---
+++ BOJ_N-18247.py
@@ -0,0 +1,12 @@
+def main():
+    T = int(input())
+    for _ in range(T):
+        N, M = map(int, input().split())
+        if N < 12 or M < 4:
+            print(-1)
+        else:
+            print(M * 11 + 4)
+
+
+if __name__ == "__main__":
+    main()
