3d4c0d657d281b30ac2ae5f6dbb10d10bc54396c
TopGun1405 <fstopgun0317@gmail.com>
2023-08-08 22:23:22 +0900

    2023-08-08

--- BOJ_N-1000-1499/BOJ_N-1343.py
+++ BOJ_N-1000-1499/BOJ_N-1343.py
@@ -1,6 +1,9 @@
 def main():
     board = input()
 
+    board = board.replace("XXXX", "AAAA").replace("XX", "BB")
+    print(-1 if 'X' in board else board)
+
 
 if __name__ == "__main__":
     main()
