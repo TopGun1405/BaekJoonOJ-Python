ac50ee3b8907421f5694ec6d607b7c6b69c471fe
TopGun1405 <fstopgun0317@gmail.com>
2023-09-04 15:53:34 +0900

    2023-09-04

--- BOJ_N-1000-1499/BOJ_N-1107.py
+++ BOJ_N-1000-1499/BOJ_N-1107.py
@@ -1,11 +1,16 @@
 def main():
     N = int(input())
     M = int(input())
-    if M == 0:
-        print(0 if N == 100 else min(abs(N - 100), len(str(N))))
-    else:
-        broken = set(map(int, input().split()))
-        unbroken = set(range(10)) - broken
+    broken = {} if M == 0 else set(map(int, input().split()))
+    minC = abs(N - 100)
+    for num in range(1_000_000):
+        num = str(num)
+        for n in num:
+            if int(n) in broken:
+                break
+        else:
+            minC = min(minC, abs(N - int(num)) + len(num))
+    print(minC)
 
 
 if __name__ == "__main__":
