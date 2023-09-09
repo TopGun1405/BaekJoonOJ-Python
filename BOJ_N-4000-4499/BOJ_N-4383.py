c7c47f9c313f759eac497d3592c99320e1b89993
TopGun1405 <fstopgun0317@gmail.com>
2023-09-09 22:31:27 +0900

    2023-09-09

--- BOJ_N-4000-4499/BOJ_N-4383.py
+++ BOJ_N-4000-4499/BOJ_N-4383.py
@@ -5,8 +5,8 @@ def main():
         except EOFError:
             break
         n, nums = nums[0], nums[1:]
-        isJolly = set(map(lambda e: abs(e[1] - e[0]), zip(nums[:-1], nums[1:])))
         jolly = set(range(1, n))
+        isJolly = set(map(lambda e: abs(e[1] - e[0]), zip(nums[:-1], nums[1:])))
         print("Jolly" if jolly & isJolly == jolly else "Not jolly")
 
 
