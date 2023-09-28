e2028e1a9abcc618ef0e8d4e8c24bf5770eeb152
TopGun1405 <fstopgun0317@gmail.com>
2023-09-28 21:55:11 +0900

    2023-09-28

--- BOJ_N-29500-29999/BOJ_N-29725.py
+++ BOJ_N-29500-29999/BOJ_N-29725.py
@@ -1,22 +1,21 @@
 def main():
-  board = [input() for _ in range(8)]
-  white = {'K': 0, 'P': 1, 'N': 3, 'B': 3, 'R': 5, 'Q': 9}
-  black = {'k': 0, 'p': 1, 'n': 3, 'b': 3, 'r': 5, 'q': 9}
-  
-  scoreW, scoreB = 0, 0
-  for r in board:
-    for c in r:
-      try:
-        scoreW += white[c]
-      except KeyError:
-        pass
-      try:
-        scoreB += black[c]
-      except KeyError:
-        pass
-  print(scoreW - scoreB)
+    board = [input() for _ in range(8)]
+    white = {'K': 0, 'P': 1, 'N': 3, 'B': 3, 'R': 5, 'Q': 9}
+    black = {'k': 0, 'p': 1, 'n': 3, 'b': 3, 'r': 5, 'q': 9}
+
+    scoreW, scoreB = 0, 0
+    for r in board:
+        for c in r:
+            try:
+                scoreW += white[c]
+            except KeyError:
+                pass
+            try:
+                scoreB += black[c]
+            except KeyError:
+                pass
+    print(scoreW - scoreB)
 
 
 if __name__ == "__main__":
-  main()
-  
+    main()
