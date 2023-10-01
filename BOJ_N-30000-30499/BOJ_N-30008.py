92615b0d4e6f90a82f102786453f0f9c880a182b
TopGun1405 <fstopgun0317@gmail.com>
2023-10-01 20:41:26 +0900

    2023-10-01

---
+++ BOJ_N-30000-30499/BOJ_N-30008.py
@@ -0,0 +1,29 @@
+def main():
+    N, K = map(int, input().split())
+    G = list(map(int, input().split()))
+    D = []
+    for Gi in G:
+        P = Gi * 100 // N
+        if 0 <= P <= 4:
+            D.append(1)
+        elif 4 < P <= 11:
+            D.append(2)
+        elif 11 < P <= 23:
+            D.append(3)
+        elif 23 < P <= 40:
+            D.append(4)
+        elif 40 < P <= 60:
+            D.append(5)
+        elif 60 < P <= 77:
+            D.append(6)
+        elif 77 < P <= 89:
+            D.append(7)
+        elif 89 < P <= 96:
+            D.append(8)
+        elif 96 < P <= 100:
+            D.append(9)
+    print(*D)
+
+
+if __name__ == "__main__":
+    main()
---
+++ BOJ_N-30000-30499/BOJ_N-30214.py
@@ -0,0 +1,7 @@
+def main():
+    s1, s2 = map(int, input().split())
+    print('E' if s1 >= s2 / 2 else 'H')
+
+
+if __name__ == "__main__":
+    main()
