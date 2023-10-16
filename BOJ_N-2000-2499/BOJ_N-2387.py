e37f718c4fa66d43af08069e2bf249e9d3882206
TopGun1405 <fstopgun0317@gmail.com>
2023-10-16 21:44:07 +0900

    2023-10-16

---
+++ BOJ_N-2000-2499/BOJ_N-2387.py
@@ -0,0 +1,29 @@
+def main():
+    # Algol 68
+    # print(("                     .", newline));
+    # print(("                    / V\",newline));
+    # print(("                  / `  /", newline));
+    # print(("                 <<   |", newline));
+    # print(("                 /    |", newline));
+    # print(("               /      |", newline));
+    # print(("             /        |", newline));
+    # print(("           /    \  \ /", newline));
+    # print(("          (      ) | |", newline));
+    # print(("  ________|   _/_  | |", newline));
+    # print(("<__________\______)\__)", newline))
+    
+    print("                     .")
+    print("                    / V\\")
+    print("                  / `  /")
+    print("                 <<   |")
+    print("                 /    |")
+    print("               /      |")
+    print("             /        |")
+    print("           /    \\  \\ /")
+    print("          (      ) | |")
+    print("  ________|   _/_  | |")
+    print("<__________\\______)\\__)")
+
+
+if __name__ == "__main__":
+    main()
