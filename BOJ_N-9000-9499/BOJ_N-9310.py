4cf36ff9fb64f66cb46e87764102766f324500a7
TopGun1405 <fstopgun0317@gmail.com>
2023-12-22 23:58:49 +0900

    2023-12-22

--- BOJ_N-9000-9499/BOJ_N-9310.py
+++ BOJ_N-9000-9499/BOJ_N-9310.py
@@ -3,13 +3,13 @@ def main():
         N = int(input())
         if N == 0:
             break
-        a, b, c = map(int, input().split())
-        if b - a == c - b:
-            d = b - a
-            print((N * (2 * a + (N - 1) * d)) // 2)
-        elif b / a == c / b:
-            r = b // a
-            print(a * (r ** N - 1) // (r - 1))
+        a1, a2, a3 = map(int, input().split())
+        if a2 - a1 == a3 - a2:
+            d = a2 - a1
+            print((N * (2 * a1 + (N - 1) * d)) // 2)
+        elif a2 / a1 == a3 / a2:
+            r = a2 // a1
+            print(a1 * (r ** N - 1) // (r - 1))
 
 
 if __name__ == "__main__":
