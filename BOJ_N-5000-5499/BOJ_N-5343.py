da8320d3ca01615e8a3f5e7f9ee9535f09cb30b7
TopGun1405 <fstopgun0317@gmail.com>
2024-01-15 23:18:31 +0900

    2024-01-15

--- BOJ_N-5000-5499/BOJ_N-5343.py
+++ BOJ_N-5000-5499/BOJ_N-5343.py
@@ -1,5 +1,16 @@
 def main():
-    pass
+    n = int(input())
+    for _ in range(n):
+        data = input()
+        error = 0
+        for i in range(0, len(data), 8):
+            oneBit = data[i:i+8][:-1].count("1")
+            parityBit = data[i:i+8][-1]
+            if oneBit % 2 == 1 and parityBit == "0":
+                error += 1
+            elif oneBit % 2 == 0 and parityBit == "1":
+                error += 1
+        print(error)
 
 
 if __name__ == "__main__":
