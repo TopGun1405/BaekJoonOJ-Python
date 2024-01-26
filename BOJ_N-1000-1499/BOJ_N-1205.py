11aac62eca5a93bba02ae33be1fa9effda840ac3
TopGun1405 <fstopgun0317@gmail.com>
2024-01-26 22:57:15 +0900

    2024-01-26

--- BOJ_N-1000-1499/BOJ_N-1205.py
+++ BOJ_N-1000-1499/BOJ_N-1205.py
@@ -1,9 +1,20 @@
 def main():
     N, C, P = map(int, input().split())
-    score = list(map(int, input().split()))
 
-    if len(score) < P:
-        score.append
+    if N == 0:
+        print(1)
+    else:
+        scores = list(map(int, input().split()))
+
+        if N == P and scores[-1] >= C:
+            print(-1)
+        else:
+            rank = N + 1
+            for i, score in enumerate(scores):
+                if score <= C:
+                    rank = i + 1
+                    break
+            print(rank)
 
 
 if __name__ == "__main__":
