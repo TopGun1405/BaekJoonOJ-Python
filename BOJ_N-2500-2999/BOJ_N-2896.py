ee0ac3057fbddfe12bc6f705a6ae9464be21b72e
TopGun1405 <fstopgun0317@gmail.com>
2024-02-06 22:16:41 +0900

    2024-02-06

---
+++ BOJ_N-2500-2999/BOJ_N-2896.py
@@ -0,0 +1,10 @@
+def main():
+    A, B, C = map(int, input().split())
+    I, J, K = map(int, input().split())
+
+    x = min(A/I, B/J, C/K)
+    print(A-I*x, B-J*x, C-K*x)
+
+
+if __name__ == "__main__":
+    main()
