f305370a5f73205a556e17ad1bde0963d6e0f5c0
TopGun1405 <fstopgun0317@gmail.com>
2024-02-15 21:56:39 +0900

    2024-02-15

---
+++ BOJ_N-23000-23499/BOJ_N-23027.py
@@ -0,0 +1,17 @@
+def main():
+    S = input()
+
+    if 'A' in S:
+        S = S.replace('B', 'A').replace('C', 'A').replace('D', 'A').replace('F', 'A')
+    elif 'B' in S:
+        S = S.replace('C', 'B').replace('D', 'B').replace('F', 'B')
+    elif 'C' in S:
+        S = S.replace('D', 'C').replace('F', 'C')
+    else:
+        S = 'A' * len(S)
+
+    print(S)
+
+
+if __name__ == "__main__":
+    main()
