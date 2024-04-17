1e550c13bd885f3eee8627bfc6a130434bfb5b5d
TopGun1405 <fstopgun0317@gmail.com>
2024-04-17 14:21:53 +0900

    2024-04-17

--- BOJ_N-1000-1499/BOJ_N-1167.py
+++ BOJ_N-1000-1499/BOJ_N-1167.py
@@ -1,18 +1,18 @@
 import sys
-sys.setrecursionlimit(10 ** 5)
+sys.setrecursionlimit(10 ** 3)
 
 
 def main():
     def dfs(node, weight):
-        left, right = 0, 0
+        if weight > dist['MAX_VAL']:
+            dist['MAX_VAL'] = weight
+            dist['MAX_NODE'] = node
+
         for nextNode, nextWeight in graph[node]:
-            w = dfs(nextNode, nextWeight)
-            if left <= right:
-                left = max(left, w)
-            else:
-                right = max(right, w)
-        dist['MAX'] = max(dist['MAX'], left + right)
-        return max(left + weight, right + weight)
+            if visited[nextNode]:
+                continue
+            visited[nextNode] = True
+            dfs(nextNode, weight + nextWeight)
 
     V = int(input())
     graph = [[] for _ in range(V + 1)]
@@ -22,9 +22,18 @@ def main():
         for e, v in zip(info[1:-1:2], info[2:-1:2]):
             graph[s].append([e, v])
 
-    dist = {'MAX': 0}
+    # print(graph)
+    dist = {'MAX_VAL': 0, 'MAX_NODE': 0}
+
+    visited = [False] * (V + 1)
+    visited[1] = True
     dfs(1, 0)
-    print(dist['MAX'])
+
+    visited = [False] * (V + 1)
+    visited[dist['MAX_NODE']] = True
+    dfs(dist['MAX_NODE'], 0)
+
+    print(dist['MAX_VAL'])
 
 
 if __name__ == "__main__":
