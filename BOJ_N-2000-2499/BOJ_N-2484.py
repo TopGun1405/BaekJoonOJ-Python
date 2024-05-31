e5aca2c9b72a2c688f7197874b39156aeb8dbb2d
TopGun1405 <fstopgun0317@gmail.com>
2024-05-31 23:39:19 +0900

    2024-05-31

--- BOJ_N-2000-2499/BOJ_N-2484.py
+++ BOJ_N-2000-2499/BOJ_N-2484.py
@@ -1,21 +1,28 @@
 def main():
     N = int(input())
-    m = 0
+
+    prices = []
     for _ in range(N):
-        dice = map(int, input().split())
-        e = dict()
-        for i in dice:
-            if i in e:
-                e[i] += 1
+        dice = sorted(map(int, input().split()))
+
+        price = 0
+        if len(set(dice)) == 1:
+            price = 50_000 + 5_000 * dice[0]
+        elif len(set(dice)) == 2:
+            if dice[1] == dice[2]:
+                price = 10_000 + 1_000 * dice[1]
             else:
-                e.update({i: 1})
-        print(e)
-        i = list(e.keys())
-        if len(e) == 1:
-            print(50000 + e[i[0]] * 5000)
-        elif len(e) == 2:
-            if len(set(e.values())) == 2:
-                print()
+                price = 2_000 + (dice[1] + dice[2]) * 500
+        elif len(set(dice)) == 4:
+            price = dice[-1] * 100
+        else:
+            for i in range(3):
+                if dice[i] == dice[i+1]:
+                    price = 1_000 + 100 * dice[i]
+                    break
+        prices.append(price)
+
+    print(max(prices))
 
 
 if __name__ == "__main__":
