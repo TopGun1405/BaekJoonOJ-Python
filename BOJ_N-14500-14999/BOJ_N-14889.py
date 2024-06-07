5f190f9e155da29c3ca6ce7ca4a03e8beb4b0d70
TopGun1405 <fstopgun0317@gmail.com>
2024-06-07 16:12:06 +0900

    2024-06-07

--- BOJ_N-14500-14999/BOJ_N-14889.py
+++ BOJ_N-14500-14999/BOJ_N-14889.py
@@ -1,32 +1,34 @@
-from itertools import combinations
-
-
 def main():
 
-    def StartLink():
-        pass
+    def StartLink(rec, idx):
+        if rec == N//2:
+            # print(visited)
+            sP, lP = 0, 0
+            for i in range(N):
+                for j in range(N):
+                    if i == j:
+                        continue
+                    if visited[i] and visited[j]:
+                        sP += S[i][j]
+                    elif not visited[i] and not visited[j]:
+                        lP += S[i][j]
+            val['MIN'] = min(val['MIN'], abs(sP - lP))
+            return
+
+        for i in range(idx, N):
+            if not visited[i]:
+                visited[i] = True
+                StartLink(rec+1, i+1)
+                visited[i] = False
 
     N = int(input())
     S = [list(map(int, input().split())) for _ in range(N)]
 
-    player = list(range(N))
-    teams = list(combinations(player, N//2))
-    print(teams)
-    minVal = 100
-    for n in range(len(teams) // 2):
-        start, link = teams[n], teams[-n-1]
-        sPoint = 0
-        for i in start[:-1]:
-            for j in start[1:]:
-                sPoint += S[i][j] + S[j][i]
-        lPoint = 0
-        for i in link[:-1]:
-            for j in link[1:]:
-                lPoint += S[i][j] + S[j][i]
-        minVal = min(minVal, abs(sPoint - lPoint))
-        print(sPoint, lPoint)
-        print(start, link)
-    print(minVal)
+    visited = [False] * N
+    val = {'MIN': 100}
+
+    StartLink(0, 0)
+    print(val['MIN'])
 
 
 if __name__ == "__main__":
