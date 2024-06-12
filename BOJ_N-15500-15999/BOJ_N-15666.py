d94a6abd85ba61c7e2c0e2dab66cf716e088a970
TopGun1405 <fstopgun0317@gmail.com>
2024-06-12 22:42:36 +0900

    2024-06-12

--- BOJ_N-15500-15999/BOJ_N-15666.py
+++ BOJ_N-15500-15999/BOJ_N-15666.py
@@ -1,23 +1,23 @@
 def main():
 
-    def dfs(n, m):
-        # #########
-        if len(t) == m:
-            print(*t)
+    def NandM():
+        if len(nums) == M:
+            print(*nums)
             return
-        seq = 0
+
+        latest = 0
         for i in range(N):
-            if (t and nums[i] < t[-1]) or seq == nums[i]:
-                continue
-            t.append(nums[i])
-            seq = nums[i]
-            dfs(n, m)
-            t.pop()
+            if latest != A[i]:
+                latest = A[i]
+                nums.append(A[i])
+                NandM()
+                nums.pop()
 
     N, M = map(int, input().split())
-    nums = sorted(map(int, input().split()))
-    t = []
-    dfs(N, M)
+    A = sorted(map(int, input().split()))
+
+    nums = []
+    NandM()
 
 
 if __name__ == "__main__":
