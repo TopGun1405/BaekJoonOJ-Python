37778a3a585b858ab28a8003fed4ccda69f08309
TopGun1405 <fstopgun0317@gmail.com>
2024-06-22 20:55:59 +0900

    2024-06-22

--- BOJ_N-31500-31999/BOJ_N-31962.py
+++ BOJ_N-31500-31999/BOJ_N-31962.py
@@ -3,10 +3,13 @@ def main():
     bus = []
     for _ in range(N):
         S, T = map(int, input().split())
+        # if S + T <= X:
+        #     bus.append(S)
         bus.append([S, T, S+T])
 
     bus = list(filter(lambda n: n[2] <= X, bus))
 
+    # print(sorted(bus)[-1] if bus else -1)
     print(sorted(bus, key=lambda k: k[0])[-1][0] if bus else -1)
 
 
