0d0b3c387bb71affc18c296b5377b8ac06060bb9
TopGun1405 <fstopgun0317@gmail.com>
2024-08-07 15:19:39 +0900

    2024-08-06

--- BOJ_N-1500-1999/BOJ_N-1912.py
+++ BOJ_N-1500-1999/BOJ_N-1912.py
@@ -2,7 +2,7 @@ def main():
     n = int(input())
     a = list(map(int, input().split()))
 
-    # memorization
+    # memoization
     for i in range(1, n):
         a[i] = max(a[i], a[i] + a[i-1])
 
