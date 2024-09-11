dfc55c3d8b39d33436d9186b2d52b2b9f6e1f680
TopGun1405 <fstopgun0317@gmail.com>
2024-09-11 15:14:35 +0900

    2024-09-11

---
+++ BOJ_N-32000-32499/BOJ_N-32260.py
@@ -0,0 +1,12 @@
+def main():
+    # #include "aplusb.h"
+    #
+    # int sum(int A, int B) {
+    #     return A + B;
+    # }
+    sum = lambda A, B: A + B
+    print(sum(1, 1))
+
+
+if __name__ == "__main__":
+    main()
