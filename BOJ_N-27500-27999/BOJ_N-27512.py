1bc405028c64cc23fe3c71e491ca6a1c04629383
TopGun1405 <fstopgun0317@gmail.com>
2025-02-15 21:54:05 +0900

    2025-02-15

---
+++ BOJ_N-27500-27999/BOJ_N-27512.py
@@ -0,0 +1,10 @@
+def main():
+    n, m = map(int, input().split())
+    if n % 2 == m % 2 == 1:
+        print(n * m - 1)
+    else:
+        print(n * m)
+
+
+if __name__ == "__main__":
+    main()
