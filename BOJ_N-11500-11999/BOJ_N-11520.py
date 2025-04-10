e5d663d2de7b3c28c512272198368df3008d7352
TopGun1405 <fstopgun0317@gmail.com>
2025-04-10 11:00:35 +0900

    2025-04-10

--- BOJ_N-11500-11999/BOJ_N-11520.py
+++ BOJ_N-11500-11999/BOJ_N-11520.py
@@ -1,27 +1,8 @@
-def make_pi(dPoint):
-    q, r, t, k, m, x = 1, 0, 1, 1, 3, 3
-    for j in range(dPoint):
-        if 4 * q + r - t < m * t:
-            yield m
-            q, r, t, k, m, x = 10*q, 10*(r-m*t), t, k, (10*(3*q+r))//t - 10*m, x
-        else:
-            q, r, t, k, m, x = q*k, (2*q+r)*x, t*x, k+1, (q*(7*k+2)+r*x)//(t*x), x+2
-
-
 def main():
-    my_array = []
-
-    for i in make_pi(1_000):
-        my_array.append(str(i))
-
-    # my_array = my_array[:1] + ['.'] + my_array[1:]
-    big_string = "".join(my_array)
-    print("here is a big string:\n %s" % big_string)
-    # N = int(input())
-    # for _ in range(N):
-    #     # Q >= P이고 π의 Q번째 자리가 D인 최소 자리 Q
-    #     # R > Q이고 π의 R번째 자리가 5인 최소 자리 R
-    #     P, D = map(int, input().split())
+    N = int(input())
+    for _ in range(N):
+        P, D = map(int, input().split())
+        print(f"{D} 5")
 
 
 if __name__ == "__main__":
