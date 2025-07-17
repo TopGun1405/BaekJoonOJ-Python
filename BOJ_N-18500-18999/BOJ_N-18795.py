2b43eff94e04cf615994811d88973cdf850c5c96
TopGun1405 <91329799+TopGun1405@users.noreply.github.com>
2025-07-17 09:41:43 +0900

    2025-07-17

---
+++ BOJ_N-18500-18999/BOJ_N-18795.py
@@ -0,0 +1,10 @@
+def main():
+    N, M = map(int, input().split())
+    A = list(map(int, input().split()))
+    B = list(map(int, input().split()))
+
+    print(sum(A) + sum(B))
+
+
+if __name__ == "__main__":
+    main()
