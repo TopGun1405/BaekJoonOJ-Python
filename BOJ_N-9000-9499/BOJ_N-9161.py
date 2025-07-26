f30a48b6b6f6878effcd6a036bb9f5545c438838
TopGun1405 <91329799+TopGun1405@users.noreply.github.com>
2025-07-26 22:38:52 +0900

    2025-07-26

---
+++ BOJ_N-9000-9499/BOJ_N-9161.py
@@ -0,0 +1,12 @@
+def main():
+    for i in range(10, 100):
+        for j in range(1, 10):
+            for k in range(10, 100):
+                x = i*10 + j
+                y = j*100 + k
+                if x/y == i/k and (x != y or i != k):
+                    print(f"{x} / {y} = {i} / {k}")
+
+
+if __name__ == "__main__":
+    main()
