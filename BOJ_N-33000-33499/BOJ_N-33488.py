37f2a9392a891f7e9f74a4dd6d946258fc72af5e
TopGun1405 <91329799+TopGun1405@users.noreply.github.com>
2025-12-07 22:02:49 +0900

    2025-12-07

---
+++ BOJ_N-33000-33499/BOJ_N-33488.py
@@ -0,0 +1,11 @@
+def main():
+    T = int(input())
+    for _ in range(T):
+        N = int(input())
+
+        print("YES")
+        print(*range(1, N+1))
+
+
+if __name__ == "__main__":
+    main()
