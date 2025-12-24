c00de8ee693962dc6301bfb284310b7b21287621
TopGun1405 <91329799+TopGun1405@users.noreply.github.com>
2025-12-24 09:02:54 +0900

    2025-12-23

--- BOJ_N-34500-34999/BOJ_N-34935.py
+++ BOJ_N-34500-34999/BOJ_N-34935.py
@@ -2,6 +2,7 @@ def main():
     N = int(input())
     A = list(map(int, input().split()))
 
+    # print(1 if len(A) == len(set(A)) else 0)
     prev = A[0]
     for i in range(1, N):
         if A[i] == prev:
