9523b7edaff85d84ae52e843cc8320175a5818c6
TopGun1405 <91329799+TopGun1405@users.noreply.github.com>
2026-02-06 10:22:45 +0900

    2026-02-06

--- BOJ_N-34500-34999/BOJ_N-34510.py
+++ BOJ_N-34500-34999/BOJ_N-34510.py
@@ -2,15 +2,6 @@ def main():
     H1, H2, H3 = map(int, input().split())
     N = int(input())
 
-    # N % 2 == 0
-    # H3 * N + H2 * (N - 1)
-    # N % 2 == 1
-    # H1 + H2 * (N - 1) + H3 * N
-    # 1 : 1
-    # 3 : 2
-    # 5 : 3
-    # 7 : 4
-
     if N % 2 == 0:
         print(H2 * (N // 2) + H3 * N)
     else:
