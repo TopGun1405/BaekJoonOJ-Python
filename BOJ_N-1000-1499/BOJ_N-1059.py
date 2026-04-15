26e46d50188a649567e204634b584b433882d2d7
TopGun1405 <91329799+TopGun1405@users.noreply.github.com>
2026-04-15 23:23:15 +0900

    2026-04-15

---
+++ BOJ_N-1000-1499/BOJ_N-1059.py
@@ -0,0 +1,22 @@
+def main():
+    L = int(input())
+    S = sorted(map(int, input().split()))
+    n = int(input())
+
+    if n in S:
+        print(0)
+    else:
+        minN, maxN = 1, 0
+        for S_i in S:
+            if S_i < n:
+                minN = S_i
+            elif S_i > n and maxN == 0:
+                maxN = S_i
+        minN += 1
+        maxN -= 1
+
+        print((n - minN) * (maxN - n + 1) + (maxN - n))
+
+
+if __name__ == "__main__":
+    main()
