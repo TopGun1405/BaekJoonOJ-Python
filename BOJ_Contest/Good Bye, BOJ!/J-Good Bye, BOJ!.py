75a67fa2c4bb8b89712c27511b45bec0625a7a32
TopGun1405 <91329799+TopGun1405@users.noreply.github.com>
2026-04-26 21:56:20 +0900

    2026-04-26

---
+++ BOJ_Contest/Good Bye, BOJ!/J-Good Bye, BOJ!.py
@@ -0,0 +1,14 @@
+def main():
+    S = "Hello, BOJ!\n"
+    S += "BOJ는 저의 하루이자 일상이고 루틴이었습니다.\n"
+    S += "Baekjoon 덕분에 지금의 더 나은 제가 있을 수 있었습니다.\n"
+    S += "다시 만날 날을 기다리며, 그동안 진심으로 감사했습니다.\n"
+    S += "Arrvederci 백준.\n"
+    S += "Good Bye, BOJ!\n"
+    S += "- topgun"
+
+    print(S)
+
+
+if __name__ == "__main__":
+    main()
